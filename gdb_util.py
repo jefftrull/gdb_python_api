9b8090da06c40f62d7f251369ad8404d8c33c15d
Jeff Trull <jeff.trull@ciere.com>
2018-02-28 21:22:17 -0800

    Include args in stack display; use nice colors

--- gdb_util.py
+++ gdb_util.py
@@ -22,44 +22,50 @@ class StackPrinter:
         if not self._frame.type() == gdb.NORMAL_FRAME:
             # IDK what to do
             return result
-        result = result + "\npc = " + '{:02X}'.format(self._frame.pc())
-        result = result + "\nsp = " + str(self._frame.read_register('sp'))
 
         locls = self.__stackmap(self._decorator.frame_locals())
-
-        result = result + "\nLOCALS"
-        for addr,symlist in locls.items():
-            result = result + "\n" + '0x{:02x}'.format(addr)
-            result = result + ",".join([" " + str(sym.name) + " (" + str(sym.type.sizeof) + ")" for sym in symlist])
-
         args = self.__stackmap(self._decorator.frame_args())
-
-        result = result + "\nARGS"
-        for addr,symlist in args.items():
-            result = result + "\n" + '0x{:02x}'.format(addr)
-            result = result + ",".join([" " + str(sym.name) + " (" + str(sym.type.sizeof) + ")" for sym in symlist])
 
         # assuming we are built with -fno-omit-frame-pointer here.  Not sure how to access
         # debug info that could tell us more, otherwise. More info is clearly present in C
         # (otherwise "info frame" could not do its job).
 
+        # Display args
+        yellow = "\u001b[33m"
+        reset_color = "\u001b[0m"
+
+        # find the address range of our args
+        # from there to *(rbp+0x8), exclusive, is the range of possible args
+        if args.keys():
+            first_arg_addr = max(args.keys())    # the one with the highest address
+            for addr in range(first_arg_addr,
+                              self._frame.read_register('rbp')+0x8,
+                              -0x8):
+                addr_hex = '0x{:02x}'.format(addr)
+                result = result + "\n" + addr_hex
+                # Is this address associated with an arg?
+                if addr in args:
+                    result = result + " " + yellow + ",".join([sym.name for sym in args[addr]]) + reset_color
+
         # *(rbp+0x8) is the stored old IP
+        cyan = "\u001b[36m"
         result = result + "\n" + str(self._frame.read_register('rbp')+0x8) + " return address"
         voidstarstar = gdb.lookup_type("void").pointer().pointer()
-        result = result + " (" + str((self._frame.read_register('rbp')+0x8).cast(voidstarstar).dereference()) + ")"
+        result = result + cyan + " (" + str((self._frame.read_register('rbp')+0x8).cast(voidstarstar).dereference()) + ")" + reset_color
 
         # *(rbp) is the old RBP
         result = result + "\n" + str(self._frame.read_register('rbp')+0x0) + " saved rbp"
 
         # print rest of stack, displaying locals
+        green = "\u001b[32m"
         for addr in range(self._frame.read_register('rbp')-0x8,
                           self._frame.read_register('sp')-0x8,
                           -0x8):
             addr_hex = '0x{:02x}'.format(addr)
             result = result + "\n" + addr_hex
-            # Is this address associated with a local?
             if addr in locls:
-                result = result + " " + ",".join([sym.name for sym in locls[addr]])
+                result = result + " " + green + ",".join([sym.name for sym in locls[addr]]) + reset_color
+        result = result + cyan + " <<< top of stack" + reset_color
 
         return result
 
