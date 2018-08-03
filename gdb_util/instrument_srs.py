3ba0e4363dee8e9b088af6027eb70b1f7ef249f2
Jeff Trull <jeff.trull@ciere.com>
2018-08-03 14:04:04 -0700

    Refactor animation toward goal state

--- gdb_util/instrument_srs.py
+++ gdb_util/instrument_srs.py
@@ -20,21 +20,30 @@ class SwapAnimation(QMainWindow):
         self.timer.timeout.connect(self._timeout)
         self.timer.start(1000)
 
-
     def _timeout(self):
         self.setStyleSheet('background-color: brown')
 
 
 class GuiThread(Thread):
-    def __init__(self):
+    def __init__(self, vec):
         Thread.__init__(self)
-        self.commands = Queue()
+        self.vec = vec              # the vector we are monitoring
+        self.messages = Queue()     # cross-thread communication
 
-    def _check_for_commands(self):
+    # next, something to do when we swap
+    def show_swap(self, a, b):  # Python values being swapped
+        # notify GUI of swap event
+        self._send_message('swapping values at %s (%s) and %s(%s)'%(
+            a.address,a.referenced_value(),b.address,b.referenced_value()))
+
+    def _send_message(self, msg): # for talking to our thread
+        self.messages.put(msg)   # contents are swap info
+
+    def _check_for_messages(self):
         # poll command queue
         # not ideal but safe. OK for now.
-        if not self.commands.empty():
-            cmd = self.commands.get()
+        if not self.messages.empty():
+            cmd = self.messages.get()
             print('got command %s'%cmd)
 
     def run(self):
@@ -45,7 +54,7 @@ class GuiThread(Thread):
 
         # periodically poll command queue
         self.cmd_poll_timer = QTimer()
-        self.cmd_poll_timer.timeout.connect(self._check_for_commands)
+        self.cmd_poll_timer.timeout.connect(self._check_for_messages)
         self.cmd_poll_timer.start(100)   # 100ms doesn't seem too terrible *shrug*
 
         self.top = SwapAnimation()
@@ -66,23 +75,25 @@ sort_bp = gdb.Breakpoint('std::sort<std::vector<int_wrapper_t, std::allocator<in
 # https://sourceware.org/bugzilla/show_bug.cgi?id=22731
 # instead we have to write out a script to a tempfile... groan...
 tf = tempfile.NamedTemporaryFile(mode='w', delete=False)
-tf.write('commands %d\nenable %d\nc\nend\n'%(sort_bp.number, swap_bp.number))
-tf.write('commands %d\npy gdb_util.instrument_srs._show_swap()\nc\nend\n'%swap_bp.number)
+
+# actions for when we arrive at std::sort
+# TODO is there a way to improve this formatting?
+tf.write(("commands %d\n"
+          "py gdb_util.instrument_srs.gui = gdb_util.instrument_srs.GuiThread(gdb.selected_frame().older().read_var('A'))\n"
+          "py gdb_util.instrument_srs.gui.start()\n"
+          "enable %d\n"
+          "c\n"
+          "end\n")%(sort_bp.number, swap_bp.number))
+
+# actions for each swap()
+tf.write(("commands %d\n"
+          "py gdb_util.instrument_srs.gui.show_swap(gdb.selected_frame().read_var('a'), gdb.selected_frame().read_var('b'))\n"
+          "c\n"
+          "end\n")%swap_bp.number)
 tf.flush()
 tf.close()
 gdb.execute('source %s'%tf.name)
 os.unlink(tf.name)
 
-gui = GuiThread()
-gui.start()
-
-# next, something to do when we swap
-def _show_swap():
-    frm = gdb.selected_frame()
-    a = frm.read_var('a')
-    b = frm.read_var('b')
-    # notify GUI of swap event
-    gui.commands.put('swapping values at %s (%s) and %s(%s)'%(
-        a.address,a.referenced_value(),b.address,b.referenced_value()))
 
 
