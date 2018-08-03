d380920af0a2ffbb09d4fdd6d23e3955421ed48e
Jeff Trull <jeff.trull@ciere.com>
2018-08-02 22:41:22 -0700

    Introduce a separate GUI thread and a message queue for updating on swaps

--- gdb_util/instrument_srs.py
+++ gdb_util/instrument_srs.py
@@ -4,6 +4,54 @@
 import gdb
 import tempfile
 import os
+from threading import Thread
+from queue import Queue
+
+# animated display of operation
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+from PyQt5.QtCore import Qt, QTimer
+
+class SwapAnimation(QMainWindow):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+
+        self.setStyleSheet('background-color: yellow')
+        self.timer = QTimer()
+        self.timer.timeout.connect(self._timeout)
+        self.timer.start(1000)
+
+
+    def _timeout(self):
+        self.setStyleSheet('background-color: brown')
+
+
+class GuiThread(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.commands = Queue()
+
+    def _check_for_commands(self):
+        # poll command queue
+        # not ideal but safe. OK for now.
+        if not self.commands.empty():
+            cmd = self.commands.get()
+            print('got command %s'%cmd)
+
+    def run(self):
+        # a warning message about not being run in the main thread gets printed
+        # everything I can find suggests this is not a real issue, so long as
+        # all QObject access happens in the same thread, which it is.
+        self.app = QApplication([])
+
+        # periodically poll command queue
+        self.cmd_poll_timer = QTimer()
+        self.cmd_poll_timer.timeout.connect(self._check_for_commands)
+        self.cmd_poll_timer.start(100)   # 100ms doesn't seem too terrible *shrug*
+
+        self.top = SwapAnimation()
+        self.top.show()
+        self.app.exec_()
+
 
 # first, two breakpoints
 # my special swap, initially disabled to avoid the call to std::shuffle
@@ -25,12 +73,16 @@ tf.close()
 gdb.execute('source %s'%tf.name)
 os.unlink(tf.name)
 
+gui = GuiThread()
+gui.start()
+
 # next, something to do when we swap
 def _show_swap():
     frm = gdb.selected_frame()
     a = frm.read_var('a')
     b = frm.read_var('b')
-    print('(PY) swapping values at %s (%s) and %s(%s)'%(
+    # notify GUI of swap event
+    gui.commands.put('swapping values at %s (%s) and %s(%s)'%(
         a.address,a.referenced_value(),b.address,b.referenced_value()))
 
 
